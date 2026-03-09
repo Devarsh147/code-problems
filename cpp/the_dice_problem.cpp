6a276f2894989f855cd806de2eef8902084577f1
Devarsh147 <parshuramkardevarsh147@gmail.com>
2026-03-09 21:58:56 +0530

    dice method 1st approch

---
+++ cpp/the_dice_problem.cpp
@@ -0,0 +1,35 @@
+//given dice range from 1 - 6
+//task is to guess the number on the opposite face of the cude.
+#include <iostream>
+using namespace std;
+
+int oppofthedice(int n) {
+    int ans ;
+    if(n==1) {
+        ans = 6;
+    }else if(n==2){
+        ans = 5;
+    }else if(n==3){
+        ans = 4;
+    }else if(n==4){
+        ans = 3;
+    }else if(n==5){
+        ans = 2;
+    }else{
+        ans = 1;
+    }return ans;
+
+}
+
+
+int main () {
+    int n = 1;
+    cout << oppofthedice(n);
+    return 0;
+
+}
+// this mathod has the time complexity of O(1)
+
+// for better approch we can you 2nd method
+// by using sum of ywo sides
+
