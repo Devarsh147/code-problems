741e34ea60b6df28c772af56efa2252688108aa9
Devarsh147 <parshuramkardevarsh147@gmail.com>
2026-03-16 17:14:20 +0530

    2nd method by recursion

--- cpp/reverse_digits.cpp
+++ cpp/reverse_digits.cpp
@@ -21,3 +21,27 @@ int main () {
 }
 // output : 2654;
 
+
+
+// 2nd method using recursion O(log10n) time and O(log10n) space.
+#include <iostream>
+using namespace std;
+
+int reverseDigits1(int n , int rev = 0) {
+    //base case
+    if (n == 0) {
+        return rev;
+    }
+    // recursion case
+    int last = n % 10; // extract the last digit
+    rev = rev * 10 + last; // add the last digit to the reverse number
+    return reverseDigits1(n / 10, rev); // remove the last digit from the n.
+
+}
+
+int main () {
+    int n = 4562;
+    cout << reverseDigits1(n);
+    return 0;
+}
+// output : 2654; with TC O(log10n) and SC O(log10n) due to recursion stack.
